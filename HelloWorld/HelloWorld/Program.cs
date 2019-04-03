54150a677c9eff3629d206ea594ad0d5d593ca87
caschong <49226570+caschong@users.noreply.github.com>
2019-04-03 15:10:46 +0800

    Try Hello World
    Init

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.WriteLine("Hello World");
+                Thread.Sleep(2000);
+            }
+            Console.ReadLine();
+        }
+    }
+}
